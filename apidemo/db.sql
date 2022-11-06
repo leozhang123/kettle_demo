72f51e61299e614db9d13a6b30f0d40685157075
lazycat <l7980@msn.com>
2022-11-06 21:59:02 +0800

    Update db.sql
    tree script

--- apidemo/db.sql
+++ apidemo/db.sql
@@ -1,1 +1,7 @@
+-- zdemo.z_tree definition
 
+CREATE TABLE `z_tree` (
+  `id` bigint NOT NULL,
+  `pid` bigint NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
